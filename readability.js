448397a43b531c16358a9276f3b2fc846dd930ca
Henrik Wingerei <henrik.wingerei@bekk.no>
2014-05-13 15:59:00 +0200

    Initial commit. Added package.json and readability.js. Takes a url as query-param and returns the article content

---
+++ readability.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var read = require('node-readability')
+
+var app = express();
+
+app.get("/", function(req, res) {
+  var url = req.query.url;
+  console.log(req);
+
+  read(url, function(err, article, meta) {
+		res.json(article.content)
+	});
+
+}).listen(8000, '127.0.0.1');
